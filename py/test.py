618275e0df60add111decc81009b51a2c1a1b477
Anatoly <azs.zavyalov@gmail.com>
2023-07-04 19:15:25 -0400

    Add more tests

--- py/test.py
+++ py/test.py
@@ -38,6 +38,71 @@ class TestProgramCommute(unittest.TestCase):
         program = Program(events, 0, 5)
         self.assertEqual(False, check_commute(program, False), "FAIL!")
 
+    def test_6(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=True, variable=1, thread=1, selected=False),
+            Event(write=False, variable=1, thread=0, selected=False),
+            Event(write=True, variable=0, thread=2, selected=False),
+            Event(write=False, variable=0, thread=1, selected=False),
+            Event(write=True, variable=2, thread=2, selected=True)
+        ]
+        program = Program(events, 0, 5)
+        self.assertEqual(True, check_commute(program, False), "FAIL!")
+
+    def test_7(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=True, variable=1, thread=1, selected=False),
+            Event(write=False, variable=1, thread=0, selected=False),
+            Event(write=True, variable=0, thread=3, selected=False),
+            Event(write=False, variable=0, thread=2, selected=False),
+            Event(write=True, variable=1, thread=2, selected=True)
+        ]
+        program = Program(events, 0, 5)
+        self.assertEqual(True, check_commute(program, False), "FAIL!")
+
+    def test_8(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=False, variable=0, thread=1, selected=False),
+            Event(write=True, variable=0, thread=2, selected=True),
+            Event(write=False, variable=0, thread=1, selected=False),
+        ]
+        program = Program(events, 0, 2)
+        self.assertEqual(False, check_commute(program, False), "FAIL!")
+
+    def test_9(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=False, variable=0, thread=1, selected=False),
+            Event(write=True, variable=0, thread=2, selected=True),
+        ]
+        program = Program(events, 0, 2)
+        self.assertEqual(True, check_commute(program, False), "FAIL!")
+
+    def test_10(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=False, variable=0, thread=1, selected=False),
+            Event(write=True, variable=0, thread=2, selected=True),
+            Event(write=True, variable=1, thread=0, selected=False),
+            Event(write=False, variable=0, thread=0, selected=False),
+        ]
+        program = Program(events, 0, 2)
+        self.assertEqual(False, check_commute(program, False), "FAIL!")
+
+    def test_11(self):
+        events = [
+            Event(write=True, variable=0, thread=0, selected=True),
+            Event(write=False, variable=0, thread=1, selected=False),
+            Event(write=True, variable=0, thread=2, selected=True),
+            Event(write=True, variable=1, thread=0, selected=False),
+        ]
+        program = Program(events, 0, 2)
+        self.assertEqual(True, check_commute(program, False), "FAIL!")
+
+
 if __name__ == "__main__":
 
     unittest.main() 
